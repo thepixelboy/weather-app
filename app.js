22059f863324e454e87576a7be432f7504c0cdd8
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-08 18:05:41 +0100

    Customizing HTTP Requests

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const request = require('postman-request');
 
 const url =
-  'http://api.weatherstack.com/current?access_key=APIKEY=37.8267,-122.4233';
+  'http://api.weatherstack.com/current?access_key=APIKEY=37.8267,-122.4233&units=f';
 
-request({ url: url }, (err, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (err, response) => {
+  const weather = response.body.current;
+  const temperature = weather.temperature;
+  const feelslike = weather.feelslike;
+  const weather_descriptions = weather.weather_descriptions[0];
+
+  console.log(
+    `${weather_descriptions} .It is currently ${temperature} degrees outside. It feels like ${feelslike} degrees outside.`
+  );
 });
